d92f48eb75abb93fcbd3b13fbb508627ad7e8df5
Atmartins <at-martins@outlook.com>
2024-02-06 17:15:57 -0300

    first commit

---
+++ src/main/java/com/example/augustoboilerplate/AugustoBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.augustoboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AugustoBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AugustoBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/augustoboilerplate/AugustoBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.augustoboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AugustoBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
